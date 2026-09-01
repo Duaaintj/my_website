6db6df874ad73a301b0a6449f944385f5fb04013
Duaaintj <duaaintj@gmail.com>
2026-09-02 00:54:33 +0100

    Update console logs and messages to English

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ app.use(express.json());
 
 app.post('/api/register', (req, res) => {
     const { username, email, password } = req.body;
-    console.log("تم استقبال البيانات:", username, email);
-    res.json({ message: "تم التسجيل بنجاح!" });
+    console.log("Data has been replaced", username, email);
+    res.json({ message: "Registration successful" });
 });
 app.use(express.static('images'));
 const PORT = 3000;
 
-// للسماح بقراءة البيانات المتبادلة
 
 app.use(express.urlencoded({ extended: true }));
 
-// تشغيل الملفات الثابتة الخاصة بموقعك (HTML, CSS, Images)
+
 app.use(express.static(__dirname));
 
-// صفحة تجريبية لربط كود الخادم
+
 app.get('/api/test', (req, res) => {
-    res.json({ message: "الخادم يعمل بنجاح!" });
+    res.json({ message: "The server is working successfully" });
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.listen(PORT, () => {
-    console.log(`الخادم يعمل الآن على الرابط: http://localhost:${PORT}`);
+    console.log(`The server is now running on the link: http://localhost:${PORT}`);
 });
